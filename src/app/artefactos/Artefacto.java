409411286447035a792cf4423d557534497c4d55
Miliiplx <mperezloray@gmail.com>
2021-04-10 20:11:02 -0300

    Agrego los atributos obligatorios de la clase Artefacto.

--- src/app/artefactos/Artefacto.java
+++ src/app/artefactos/Artefacto.java
@@ -1,5 +1,20 @@
 package app.artefactos;
 
+import app.poderes.Poder; //PREGUNTAR SI ESTO VA ASI. DE LO CONTRARIO ME SALTA ERROR EN EL TIPO.
+
 public class Artefacto {
+
+    String nombre;
+    double amplificadorDeDdanio; //Numero que representa una forma de incremento de daño. De 0 a 1.
+    double amplificadorDeCuracion; //Numero que representa una forma de decremento de daño recibido. 0 a 1
+    Poder poder; //Poder de artefacto. Este atributo debe estar pero su valor es opcional.
+    //Ej CapaInvisibilidad tiene el poder de invisibilidad. Los
+    //otros son a libre interpretación.
+
+    
+
+
+
+
     
 }
