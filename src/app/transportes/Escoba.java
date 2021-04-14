483a9a1e5ea22d35ef25a1ef82c8b68297909683
Geormary27 <geormarygarcia27@gmail.com>
2021-04-13 22:27:20 -0300

    Se agregaron atributos, interfaces, getters y setters al paquete transporte.

--- src/app/transportes/Escoba.java
+++ src/app/transportes/Escoba.java
@@ -4,18 +4,40 @@ import app.interfaces.IEsMagico;
 
 public class Escoba extends Transporte implements IEsMagico {
 
-    //IMPLEMENTO LA INTERFACE
+    private int anioDeFabricacion; //Solo el año que fue creada la escoba...Nimbus2000(año 1991) /SaetaDeFuegoSuprema(año 2014)
+    private String descripcion;  //Informa que escoba tiene el jugador...MagosBuenos: Nimbus2000 / MagosMalos: SaetaDeFuegoSuprema
     
+    //Getter y Setters
+
+    public int getAnioDeFabricacion(){
+        return this.anioDeFabricacion;
+    }
+    public void setAnioDeFabricacion(int anioDeFabricacion){
+        this.anioDeFabricacion = anioDeFabricacion;
+    }
+
+    public String getDescripcion(){
+        return this.descripcion;
+    }
+    public void setDescripcion(String descripcion){
+        this.descripcion = descripcion;
+    }
+
+    //Implemento la interface
+
     @Override
     public boolean esInvisibleAMuggles() {
-        // TODO Auto-generated method stub
+
         return false;
     }
 
     @Override
     public boolean esInvisible() {
-        // TODO Auto-generated method stub
+        
         return false;
     }
+    
+   
+
     
 }
--- src/app/transportes/Transporte.java
+++ src/app/transportes/Transporte.java
@@ -1,5 +1,16 @@
 package app.transportes;
 
 public class Transporte {
+   private String nombre;
+
+   //Getters y Setters
+
+   public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
     
+
 }
